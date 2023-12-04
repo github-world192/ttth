d2dfb6c407a077beccf745f4d302082010deb8c6
github-world192 <dillen99999999@gmail.com>
2023-12-04 03:17:57 -0800

    Initial Commit

---
+++ ttth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ttth
+//
+//  Created by llll on 2023/12/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
